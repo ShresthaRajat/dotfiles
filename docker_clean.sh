49c45949d6b41207ab9a9cbb73ce08838a3b80c1
rajat.shrestha.pop <rajat.shrestha@eydean.com>
2020-07-08 20:48:45 +0545

    Added docker cleaner scripts

---
+++ docker_clean.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+
+docker system prune
+docker system prune -a
+
+docker images -a
+docker images -f dangling=true
+docker images purge
+
+docker images -a |  grep "pattern"
+docker images -a | grep "pattern" | awk '{print $3}' | xargs docker rmi
+docker images -a
+docker rmi $(docker images -a -q)
+
+
+docker ps -a
+docker run --rm image_name
+
+
+docker ps -a -f status=exited
+docker rm $(docker ps -a -f status=exited -q)
