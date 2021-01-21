626bf805b7b876b82340bf4d8b70d14d0be84535
Rajat Shrestha <shrestharajat@hotmail.com>
2021-01-21 18:45:50 +0545

    add default workspace number and alias

--- .aliases.sh
+++ .aliases.sh
@@ -231,3 +231,8 @@ alias python='python3'
 
 alias python2='python2'
 alias pip2='pip'
+alias venv='source ~/.cache/pyvenv/venv/bin/activate'
+alias newvenv='rm -rf ~/.cache/pyvenv/venv && python -m virtualenv ~/.cache/pyvenv/venv'
+alias defws='gsettings set org.gnome.desktop.wm.preferences num-workspaces 4'
+
+
