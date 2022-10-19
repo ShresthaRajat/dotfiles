b9fe47b8f89f53442a634b416eb40f61699c5926
shrestharajat <shrestharajat@hotmail.com>
2022-10-19 15:43:59 +0100

    refactor minikube script file

--- scripts/minikube.sh
+++ scripts/minikube.sh
@@ -1,16 +1,4 @@
 #!/bin/sh
-
-
-# Install minikube
-# Requires docker to be installed
-curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-sudo install minikube-linux-amd64 /usr/local/bin/minikube
-rm -rf minikube-linux-amd64
-
-# Install kubectl
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-
-
 # minikube commands
 
 # basic info
